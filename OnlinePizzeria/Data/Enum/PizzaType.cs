6817f0cf9f85302a8faa18fe6ef8d195197c94e9
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-18 13:56:21 +0200

    Add files via upload

---
+++ OnlinePizzeria/Data/Enum/PizzaType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePizzeria.Data.Enum
+{
+    public enum PizzaType
+    {
+        NonVegetarian = 1,
+        Vegetarian = 2
+    }
+}
---
+++ OnlinePizzeria/Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlinePizzeria.Data
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Address { get; init; }
+    }
+}
