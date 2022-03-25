48703948d4c0cdf004a81c007ef2056a3770ce1e
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-25 23:21:55 +0200

    Add files via upload

---
+++ OnlinePizzeria/Data/Enum/PaymentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePizzeria.Data.Enum
+{
+    public enum PaymentType
+    {
+        Cash = 1,
+        Credit = 2
+    }
+}
