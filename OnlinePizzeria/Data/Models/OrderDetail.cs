db31ee151ba302fc7dbbbcb9281273c9b876abf4
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-25 23:19:35 +0200

    Add files via upload

---
+++ OnlinePizzeria/Data/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePizzeria.Data.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; init; }
+        public int OrderId { get; init; }
+        public int PizzaId { get; init; }
+        public int Quantity { get; init; }
+        public Decimal Price { get; init; }
+        public virtual Pizza Pizza { get; init; }
+        public virtual Order Order { get; init; }
+    }
+}
---
+++ OnlinePizzeria/Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlinePizzeria.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; init; }
+        public Pizza Pizza { get; init; }
+        public int Quantity { get; set; }
+        public string ShoppingCartId { get; init; }
+    }
+}
