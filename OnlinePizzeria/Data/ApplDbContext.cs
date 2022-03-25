3b9452c25e720d0f0a5a0419eba339a5bd4964bb
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-25 23:20:15 +0200

    Update ApplDbContext.cs

--- OnlinePizzeria/Data/ApplDbContext.cs
+++ OnlinePizzeria/Data/ApplDbContext.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using OnlinePizzeria.Data.Models;
 
 namespace OnlinePizzeria.Data
 {
@@ -11,6 +9,18 @@ namespace OnlinePizzeria.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = OnlinePizzeria; Trusted_Connection = True; ");
         }
     }
 }
