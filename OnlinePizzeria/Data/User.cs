b998edf7ac483b445b07f59230394b368c302ff4
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-25 23:20:48 +0200

    Add files via upload

---
+++ OnlinePizzeria/Data/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OnlinePizzeria.Data
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
