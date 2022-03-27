b806b24520be34bded8f305a3c836c68c659442a
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-27 23:17:38 +0300

    Add files via upload

---
+++ OnlinePizzeria.Infrastructure/Common/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OnlinePizzeria.Infrastructure.Common.Contracts
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> All();
+        Task<IEnumerable<TEntity>> AllAsNoTracking();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+        Task<TEntity> FindOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChangesAsync();
+    }
+}
