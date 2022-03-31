347b4bf906c0dc08d78ab49bee42c4f45cdfcb7d
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-03-31 23:41:33 +0300

    Update Pizza.cs

--- OnlinePizzeria/Data/Models/Pizza.cs
+++ OnlinePizzeria/Data/Models/Pizza.cs
@@ -1,4 +1,4 @@
-﻿using OnlinePizzeria.Data.Enum;
+using OnlinePizzeria.Data.Enum;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,13 +9,14 @@ namespace OnlinePizzeria.Data.Models
 {
     public class Pizza
     {
-        private decimal price;
+        //private decimal price;
         public Pizza()
         {
             Feedbacks = new HashSet<Feedback>();
         }
 
-        public int Id { get; init; }
+        [Key]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
         public string Name { get; init; }
 
         [Required]
@@ -24,14 +25,12 @@ namespace OnlinePizzeria.Data.Models
 
         [DataType(DataType.ImageUrl)]
         public string ImageUrl { get; init; }
-        public int SizeId { get; init; }
-        public Size Size { get; init; }
         public Enum.Type Type { get; init; }
-        //public decimal Price { get; init; }
-        public decimal Price { get { return price; } set { price = value; } }
-        public decimal PriceForMedium { get { return price * 1.25m; } }
-        public decimal PriceForLarge { get { return price * 1.5m; } }
-        public decimal PriceForFamily { get { return price * 2; } }
+        public decimal Price { get; init; }
+        //public decimal Price { get { return price; } set { price = value; } }
+        //public decimal PriceForMedium { get { return price * 1.25m; } }
+        //public decimal PriceForLarge { get { return price * 1.5m; } }
+        //public decimal PriceForFamily { get { return price * 2; } }
         public Category Category { get; init; }
         public ICollection<Feedback> Feedbacks { get; init; }
     }
