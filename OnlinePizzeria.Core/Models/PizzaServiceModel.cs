f1ec6aee2ceb6b35298f3b93a1855b370c1fcd5c
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-04-07 22:47:46 +0300

    Add files via upload

---
+++ OnlinePizzeria.Core/Models/PizzaServiceModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlinePizzeria.Data.Enum;
+using Type = OnlinePizzeria.Data.Enum.Type;
+
+namespace OnlinePizzeria.Core.Models
+{
+    public class PizzaServiceModel
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string ImageUrl { get; init; }
+        public Type Type { get; init; }
+        public decimal Price { get; init; }
+        public Category Category { get; init; }
+    }
+}
